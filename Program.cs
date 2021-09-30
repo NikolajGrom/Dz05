c13f850da771c1a81f4ab9746b6328e3aabc6e76
NikolajGrom <solonliapota@gmail.com>
2021-09-30 12:07:49 +0300

    CommitArray

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace Dz05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--- Nobel University! ---");
+            Console.WriteLine("--- Nobel University! ---\n");
+            int[][] arrMark = new int[2][];
+
+            // Initialize the elements.
+            arrMark[0] = new int[5] { 1, 3, 5, 7, 9 };
+            arrMark[1] = new int[4] { 2, 4, 6, 8 };
+
+            // Display the array elements.
+            for (int i = 0; i < arrMark.Length; i++)
+            {
+                System.Console.Write("Element({0}): ", i);
+
+                for (int j = 0; j < arrMark[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", arrMark[i][j], j == (arrMark[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();
+            }
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
 
 
         }
