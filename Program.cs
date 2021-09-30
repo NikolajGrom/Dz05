daf40977cdf7ae3bf75c35f964c477e7dc7bcb1b
NikolajGrom <solonliapota@gmail.com>
2021-09-30 11:31:04 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dz05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
