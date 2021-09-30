b96efee4bbbac8e37a09f92fd7fa3278185664d6
NikolajGrom <solonliapota@gmail.com>
2021-09-30 13:14:39 +0300

    commitStudent2

--- Program.cs
+++ Program.cs
@@ -24,8 +24,28 @@ namespace Dz05
                 }
                 System.Console.WriteLine();
             }
+            int[][] arryMark = new int[2][];
 
-            Student student1 = new Student("Петя", "Петров", "Петрович", "2", 18, arrMark);
+            // Initialize the elements.
+            arryMark[0] = new int[3] { 1, 3, 5 };
+            arryMark[1] = new int[4] { 5, 8, 8, 8 };
+
+            // Display the array elements.
+            for (int i = 0; i < arryMark.Length; i++)
+            {
+                System.Console.Write("Element({0}): ", i);
+
+                for (int j = 0; j < arryMark[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", arryMark[i][j], j == (arryMark[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();
+            }
+            
+
+            Student student1 = new Student("Петя", "Петров", "Петрович", "PO2", 18, arrMark);
+            student1.Print();
+            Student student2 = new Student("Vasya", "Vasiliev", "Vasilevich", "PV2", 18, arrMark);
             student1.Print();
 
             // Keep the console window open in debug mode.
